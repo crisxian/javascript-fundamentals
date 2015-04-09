eba98fc9ce5990630f893dd0542831ec6eee3666
Christian <crisxian@users.noreply.github.com>
2015-04-08 19:45:58 -0700

    OOP prototypal instantiation
    - refactor stack.js to illustrate prototypal instantiation
    - passed tests

--- OOP/src/prototypal/stack.js
+++ OOP/src/prototypal/stack.js
@@ -1,5 +1,28 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance._size = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this._size] = value;
+  this._size++;
+};
+
+stackMethods.pop = function(){
+  this._size && this._size--;
+  var result = this.storage[this._size];
+  delete this.storage[this._size];
+  return result;
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
